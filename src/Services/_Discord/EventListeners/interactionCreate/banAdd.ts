0266172d2f06df7aa31d8c4585f3d5b13c8f8a18
Sarim <sarim.xs@gmail.com>
2024-07-30 14:11:18 -0600

    Add interaction source

--- src/Services/_Discord/EventListeners/interactionCreate/banAdd.ts
+++ src/Services/_Discord/EventListeners/interactionCreate/banAdd.ts
@@ -23,7 +23,7 @@ export default function listener(): void {
             const result = await suspect.ban(
               interaction.member,
               undefined,
-              reason,
+              reason ? `${reason} - source: ${interaction.user.username}` : `source: ${interaction.user.username}`,
             );
             if (result instanceof Error) {
               await interaction.editReply({
